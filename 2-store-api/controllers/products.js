f4f3604d371bd146b6c71a14cb77fdcf7388c664
Hoang Nguyen <>
2023-02-16 17:44:57 +0700

    select fields products

--- 2-store-api/controllers/products.js
+++ 2-store-api/controllers/products.js
@@ -1,7 +1,7 @@
 
 const Product = require('../models/products');
 
-
+//Test
 const getAllProductsStatic = async (req, res) => {
     const products = await Product.find({ featured: true });
     res.status(200).json({ products, nbHits: products.length })
@@ -9,16 +9,20 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     let query = req.query;
-    if(query.name) {
-        query.name = {$regex: query.name, $options: 'i'}
+    if (query.name) {
+        query.name = { $regex: query.name, $options: 'i' }
     }
-    let result = await Product.find(query);
-    if(query.sort) {
+    let result = Product.find(query);
+    if (query.sort) {
         const sortList = query.sort.split(',').join(' ');
         result = result.sort(sortList);
     }
     else {
         result = result.sort('createAt');
+    }
+    if (query.fields) {
+        const fieldsList = query.fields.split(',').join(' ');
+        result = result.select(fieldsList);
     }
     const products = await result;
     console.log(query);
