e038d34c4bc2bcf1dd66e9f7b1a10954ab8617a9
Hoang Nguyen <>
2023-02-16 20:57:13 +0700

    numeric filters

--- 2-store-api/controllers/products.js
+++ 2-store-api/controllers/products.js
@@ -12,6 +12,26 @@ const getAllProducts = async (req, res) => {
     if (query.name) {
         query.name = { $regex: query.name, $options: 'i' }
     }
+
+    if (query.numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|>=|<=|=)\b/g
+        let filters = query.numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if(options.includes(field)) {
+                query[field] = {[operator]:Number(value)};
+            }
+        })
+    }
+
     let result = Product.find(query);
     if (query.sort) {
         const sortList = query.sort.split(',').join(' ');
@@ -24,6 +44,8 @@ const getAllProducts = async (req, res) => {
         const fieldsList = query.fields.split(',').join(' ');
         result = result.select(fieldsList);
     }
+
+    
     const page = Number(query.page) || 1;
     const itemPerPage = 10;
     const limit = Number(query.limit) || itemPerPage;
