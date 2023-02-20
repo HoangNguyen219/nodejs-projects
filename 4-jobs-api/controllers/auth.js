89e0145013836a7b2d17a1eddfd7518872c302a1
Hoang Nguyen <>
2023-02-20 22:38:48 +0700

    hash user password

--- 4-jobs-api/controllers/auth.js
+++ 4-jobs-api/controllers/auth.js
@@ -1,9 +1,22 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes')
+const bcrypt = require('bcryptjs')
 
-const register = async (req,res) => {
-    res.send('register');
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+    const salt = await bcrypt.genSalt(10)
+    console.log(salt);
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const tempUser = {name, email, 
+        password: hashedPassword
+    }
+
+    const user = await User.create(tempUser)
+    console.log(user);
+    res.status(StatusCodes.CREATED).json({ user });
 }
 
-const login = async (req,res) => {
+const login = async (req, res) => {
     res.send('login');
 }
 
