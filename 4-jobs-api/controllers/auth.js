3af3fabe0608dd5d9f46a3efb20ec1baa9a82e60
Hoang Nguyen <>
2023-02-20 23:27:33 +0700

    Generate token, instance method

--- 4-jobs-api/controllers/auth.js
+++ 4-jobs-api/controllers/auth.js
@@ -1,11 +1,12 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
 
 const register = async (req, res) => {
-    const user = await User.create(req.body)
-    console.log(user);
-    res.status(StatusCodes.CREATED).json({ user });
+    const user = await User.create(req.body);
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
 const login = async (req, res) => {
