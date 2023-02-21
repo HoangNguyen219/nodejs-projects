ac6a8394b9dab060c2f3c1c41f192fb39f5e6e6a
Hoang Nguyen <>
2023-02-21 21:30:38 +0700

    validation error

--- 4-jobs-api/middlewares/error-handler.js
+++ 4-jobs-api/middlewares/error-handler.js
@@ -1,4 +1,4 @@
-const { CustomAPIError } = require('../errors');
+// const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
@@ -7,16 +7,21 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         msg: err.message || 'Something went wrong, try again later'
     }
 
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({ msg: err.message })
+    // }
 
-
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message })
+    if(err.name == 'ValidationError') {
+        customError.msg = Object.values(err.errors).map((item)=>item.message).join(',');
+        customError.statusCode = 400;
     }
+
+
     if (err.code && err.code === 11000) {
         customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
         customError.statusCode = 400;
     }
-    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ msg: err.message });
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ msg: err });
     return res.status(customError.statusCode).send({ msg: customError.msg });
 }
 
