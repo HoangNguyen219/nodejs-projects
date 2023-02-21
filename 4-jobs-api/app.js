c938c3c3abf05d26c76740198d9908745e6906f9
Hoang Nguyen <>
2023-02-21 23:48:51 +0700

    security

--- 4-jobs-api/app.js
+++ 4-jobs-api/app.js
@@ -2,6 +2,10 @@
 require('dotenv').config();
 require('express-async-errors');
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xxs-clean');
+const rateLimiter = require('express-rate-limit');
 
 const express = require('express');
 const app = express();
@@ -10,7 +14,15 @@ const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15*60*1000, //15 minutes
+    max: 100 //limit each IP to 100 requests per windowMs
+}));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //routers
 const authRouter = require('./routes/auth');
