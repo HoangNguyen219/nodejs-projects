53545f6c88f14e44efea699a12914b81990997a0
Hoang Nguyen <>
2023-02-22 13:24:26 +0700

    upload image complete & cloudinary set up

--- 5-file-upload/app.js
+++ 5-file-upload/app.js
@@ -9,6 +9,14 @@ const connectDB = require('./db/connect');
 
 const fileupload = require('express-fileupload')
 
+// USE V2!!
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
 const productRouter = require('./routes/productRoutes');
 
 const notFoundMiddleware = require('./middlewares/not-found');
--- 5-file-upload/controller/uploadsController.js
+++ 5-file-upload/controller/uploadsController.js
@@ -1,12 +1,29 @@
 const { StatusCodes } = require('http-status-codes');
 const path = require('path')
+const customError = require('../errors')
+
+
 const uploadProductImage = async (req, res) => {
+    if (!req.files) {
+        throw new customError.BadRequestError('No File Uploaded');
+    }
     const productImage = req.files.image;
+
+    if(!productImage.mimetype.startsWith('image')) {
+        throw new customError.BadRequestError('Please Upload Image');
+    }
+
+    const maxSize = 1024 * 1024;
+    if(productImage.size > maxSize) {
+        throw new customError.BadRequestError('Please upload image smaller than 1Kb')
+    }
+
+
     const imagePath = path.join(__dirname, '../public/uploads' + `/${productImage.name}`);
-    await productImage.mv(imagePath, ()=>{
+    await productImage.mv(imagePath, () => {
 
     })
-    res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
+    res.status(StatusCodes.OK).json({ image: { src: `/uploads/${productImage.name}` } })
 }
 
 
