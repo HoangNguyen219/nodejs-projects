6cb5c6331785c06af1a26230dc8bb210f63f9625
Hoang Nguyen <>
2023-02-22 12:46:49 +0700

    express-fileupload package

--- 5-file-upload/app.js
+++ 5-file-upload/app.js
@@ -7,13 +7,17 @@ const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
 
+const fileupload = require('express-fileupload')
+
 const productRouter = require('./routes/productRoutes');
 
 const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json());
+app.use(fileupload())
 
 //routes
 app.use('/api/v1/products', productRouter)
