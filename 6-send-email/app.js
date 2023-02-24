2e967f30b44f012349655c1693bdae6f85012fe5
Hoang Nguyen <>
2023-02-24 15:49:21 +0700

    set up send email

--- 6-send-email/app.js
+++ 6-send-email/app.js
@@ -5,30 +5,17 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
-const connectDB = require('./db/connect');
 
-const fileupload = require('express-fileupload')
-
-// USE V2!!
-const cloudinary = require('cloudinary').v2;
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
-
-const productRouter = require('./routes/productRoutes');
+const sendEmail = require('./controller/sendEmail');
 
 const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 //middleware
-app.use(express.static('./public'))
 app.use(express.json());
-app.use(fileupload({ useTempFiles: true }))
 
 //routes
-app.use('/api/v1/products', productRouter)
+app.get('/send', sendEmail)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -37,12 +24,14 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI);
+        // await connectDB(process.env.MONGO_URI);
         app.listen(port, console.log(`server is listening on http://localhost:${port}`))
 
     } catch (error) {
         console.log(error);
     }
 }
-
+console.log(module.exports);
+console.log(exports);
 start()
+
