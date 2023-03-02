7a516eec8911ba8c0635198ded47b56d7e16e283
Hoang Nguyen <>
2023-03-02 12:58:31 +0700

    show me

--- 7-e-commerce-api/controller/userController.js
+++ 7-e-commerce-api/controller/userController.js
@@ -1,25 +1,25 @@
 const User = require('../models/User')
-const {StatusCodes} = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 const { use } = require('express/lib/router')
 
 const getAllUsers = async (req, res) => {
     console.log(req.user);
-    const users = await User.find({role: 'user'}).select('-password')
-    res.status(StatusCodes.OK).json({users})
+    const users = await User.find({ role: 'user' }).select('-password')
+    res.status(StatusCodes.OK).json({ users })
 }
 
 const getSingleUser = async (req, res) => {
     const id = req.params.id
-    const user = await User.findOne({_id: id}).select('-password')
+    const user = await User.findOne({ _id: id }).select('-password')
     if (!user) {
         throw new CustomError.NotFoundError(`No user with id ${id}`)
     }
-    res.status(StatusCodes.OK).json({user})
+    res.status(StatusCodes.OK).json({ user })
 }
 
 const showCurrentUser = async (req, res) => {
-    res.send('current')
+    res.status(StatusCodes.OK).json({ user: req.user })
 }
 
 const updateUser = async (req, res) => {
